e05802726dafc279a248b9a76784f3e34945b08b
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-05-30 03:54:14 -0700

    commit

---
+++ week-1/gonzalez-assignment-1.4.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Juvenal Gonzalez
+; Date: 30 May 2020
+; Description: Variables are assigned names and numbers that are then outputed.
+;==================
+*/
+
+//input
+var firstName = "Juvenal";
+var lastName = "Gonzalez";
+var age = 29;
+var street = "225 Jackson St";
+var city = "San Francisco";
+var zipCode = 95207;
+//output
+console.log("Name:", firstName, lastName, "Age:", age,);
+//console.log newline creates ugly space on next line so another console.log was added
+console.log("Address:", street, ",", city, ",", zipCode);
--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Juvenal Gonzalez
+; Date: 30 May 2020
+; Description: Very simple program that outputs hello world.
+;==================
+*/
+
 console.log("hello world my name is Juvenal Gonzalez");
