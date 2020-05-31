3f759d34b6da60c3bd9d454a8d53f3d6418dc47c
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-05-31 08:37:32 -0700

    commit

---
+++ discussion1.1/gonzalez-discussion1.1b.js
@@ -0,0 +1,18 @@
+ 
+/* 
+===================================================== 
+; Title: Discussion 1.1 
+; Author: Brooklyn  Hairston
+; Date: 30 May 2020 
+; Modified By: Juvenal Gonzalez
+; Description: Correction of peers intended errors for training purposes.
+;===================================================== 
+*/ 
+
+var greeting  =  "Hello There!"; 
+var question  =  " How are you today?"; 
+//changed greet to greeting
+var text =  greeting +  question;  
+//this variable creates a line of text from both previous variables. 
+console.log ( text ); //included . after console so that the ojbect operated properly
+//total of 2 errors corrected
