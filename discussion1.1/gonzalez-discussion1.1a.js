c9c55f7adbe1df2ee94fd30e5eda5eb5820c4927
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-05-30 21:10:14 -0700

    commit

--- discussion1.1/gonzalez-discussion1.1a.js
+++ discussion1.1/gonzalez-discussion1.1a.js
@@ -7,6 +7,16 @@
 
 <script> 
 
+/* 
+===================================================== 
+; Title: Discussion 1.1 
+; Author: Brooklyn  Hairston
+; Date: 30 May 2020 
+; Modified By: Juvenal Gonzalez
+; Description: Correction of peers intended errors for training purposes.
+;===================================================== 
+*/ 
+
 /*suggestion -- start with lowercasee for variables and start
 with uppercase for Objects which is something that holds many variables of the same
 data type */
@@ -17,7 +27,7 @@ var TimeInMinutes = 1;
 var AvgCharPerWord = 5; 
 //CharactersTyped changed to charactersTyped
 var GrossWPM = (charactersTyped/AvgCharPerWord)/TimeInMinutes;  
-//very tricky quotation mark changed and + sign changed to equal
+//very tricky quotation mark changed and + sign changed to equal sign
 document.getElementById("Typing Speed").innerHTML="Your Typing Speed Is:" +GrossWPM; 
 
 //4 errors found
