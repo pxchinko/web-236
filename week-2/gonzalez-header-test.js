27a645f155bce2ddfbe91aa8425aa2626d53ec61
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-06-06 18:19:51 -0700

    commit

---
+++ week-2/gonzalez-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  gonzalez-header-test.js
+; Author: Professor Krasso
+; Date:   06 June 2020
+; Modified By: Juvenal Gonzalez
+; Description: Displays a formatted header
+;===========================================
+*/
+//Initialization of header that imports the function from the file location.
+const header = require('./gonzalez-header.js');
+//Method call that outputs the student name and assignment name in header format
+//by passing 3 string variables into the header function.
+console.log(header.display("Juvenal", "Gonzalez", "Exercise 2.2"));
---
+++ week-2/gonzalez-header.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title:  gonzalez-header.js
+; Author: Professor Krasso
+; Date:   06 June 2020
+; Modified By: Juvenal Gonzalez
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/*
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+//this code is being reused and I have only changed the title
+//function that outputs 3 string variables in proper header format.
+exports.display = function (firstName, lastName, assignment) {
+	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+	new Date().toLocaleDateString('en-US');
+
+	return output;
+}
