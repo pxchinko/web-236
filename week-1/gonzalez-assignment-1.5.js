6a61b3d84a3204fb79e585c23532552045b6988f
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-06-10 18:28:32 -0700

    commit

--- week-1/gonzalez-assignment-1.5.js
+++ week-1/gonzalez-assignment-1.5.js
@@ -3,7 +3,7 @@
 ; Title: Assignment 1.5
 ; Author: Juvenal Gonzalez
 ; Date: 30 May 2020
-; Description: Create multiple variables for 
+; Description: Create multiple variables for
 ; 3 different employees with proper format of date and decimal place.
 ;===========================================
 */
@@ -23,10 +23,16 @@ var payRate1 = 5.75;
 var payRate2 = 11.70;
 var payRate3 = 15.55;
 //date variables with slash styling and using date object
-var hireDate1 = "June\\25\\2015";
-var hireDate2 = new Date(2020,5,1);
-var hireDate3 = new Date(2010,12,15);
+//Used -1 because month uses 0-11 like array elements
+//but wanted to adopt this methodology to reduce errors
+var hireDate1 = new Date(2015, 6-1, 2);
+var hireDate2 = new Date(2010, 3-1, 15);
+var hireDate3 = new Date(2015, 12-1, 30);
 //output of 3 employees
-console.log("Name:", firstName1, lastName1, "Address:", address1, "Payrate:", payRate1.toFixed(1), "hiredate:", hireDate1);
-console.log("Name:",firstName2, lastName2, "Address:", address2, "Payrate:", payRate2.toFixed(1), "hiredate:", hireDate2);
-console.log("Name:",firstName3, lastName3, "Address:", address3, "Payrate:", payRate3.toFixed(1), "hiredate:", hireDate3);
+console.log("Name:", firstName1, lastName1, "Address:", address1, "Payrate:", payRate1.toFixed(1), "hiredate:", hireDate1.toLocaleDateString());
+console.log("Name:",firstName2, lastName2, "Address:", address2, "Payrate:", payRate2.toFixed(1), "hiredate:",hireDate2.toLocaleDateString());
+console.log("Name:",firstName3, lastName3, "Address:", address3, "Payrate:", payRate3.toFixed(1), "hiredate:", hireDate3.toLocaleDateString());
+
+// I understand this method as well and its uses hr2 for the output but i liked the above method which uses 3 lines of input
+//hireDate = new Date(2010, 4-1, 15);
+//var hr2 = hireDate.toLocaleDateString();
