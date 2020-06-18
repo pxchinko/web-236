b7293086805fccea6f09a88e21a6edc172b59642
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-06-18 15:01:51 -0700

    c

---
+++ week-4/gonzalez-discussion-edit1.js
@@ -0,0 +1,36 @@
+/*
+============================================
+; Title:  barleta-exercise-3.3.js
+; Author: Marie Nicole Barleta
+; Date:   18 June 2020
+; Modified By: Juvenal Gonzalez
+; Description: Fix multiple errors
+;===========================================
+*/
+
+
+/*
+expected output:
+2 bananas
+*/
+
+//start program
+//variable
+
+var arr = [
+  ['apple', 'mango', 'banana'],
+  ['carrots', 'potato', 'spinach'],
+  ['pasta', 'rice', 'beans']
+]
+
+//This block of code has multiple errors
+//bannan initialized at 3, changed to 2 so that output starts with 2 bananas
+for (banana=2; banana <= 7; banana++){ //changed banana =+ operator to ++ so that it increments
+  if (banana > 1){
+    console.log(banana + " " + arr[0][2]+"s")//Changed from [2][0] to target proper element
+  }                                          // in multidimensional array
+  else {
+    console.log(banana + " " + arr[0][2])   //Added numbers to array elements
+  }                                         //so that program would execute
+
+}
