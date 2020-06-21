7692c4942aa359e21c002059dfdb0561a1c4b401
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-06-21 07:49:14 -0700

    c

---
+++ week-4/gonzalez-discussion-4.1-edit2.js
@@ -0,0 +1,20 @@
+/*
+Title: gonzalez-discussion-4.1-edit2.js
+Author: Erica Perry
+Date:6/21/2020
+Modified: Juvenal Gonzalez
+Description:simple array with at least two errors
+*/
+
+//removed header file, it will not run because we do
+//not have the imported file saved on our directory
+
+// array with three elements
+var items = [ "chocoLatE" , "crackers" , "marshMallows" ];
+
+// adding a third index into the array
+
+items [ 3 ] = "fire" ; // changed Itmes to items as Itmes is undefined
+console.log( items [ 1 ]); //changed console . log to console.log for proper
+                          // spacing while calling a console object method.
+// the expected output is crackers
