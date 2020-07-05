c3e4ecc0e7536470e7eee73dfeaded638d46cf29
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-07-05 15:56:39 -0700

    c

--- week-6/gonzalez-assignment-6.4.js
+++ week-6/gonzalez-assignment-6.4.js
@@ -1,6 +1,13 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
+============================================
+; Title:  gonzalez-discussion-6.4
+; Author: Juvenal Gonzalez
+; Date:   05 July 2020
+; Description: Create nested object literal and display the results
+;===========================================
+*/
+
+/*
 
   FirstName LastName
   Assignment 6.4
@@ -8,3 +15,23 @@
 
   Ticket 105 was created on <today' date> and assigned to employee Bob Jones (Programmer I).
 */
+
+//header
+const header = require('../week-2/gonzalez-header');
+console.log(header.display("Juvenal", "Gonzalez", "Exercise 6.4"));
+
+// ticket object with four data fields and a nested object with also 4 data fields
+var ticket = {
+   id: 105,
+   name: "hardware Issue",
+   dateCreated : new Date().toLocaleDateString('en-US'),
+   priority: "high",
+  person: { //nested object
+   id: 10,
+   firstName: "Bob",
+   LastName: "Jones",
+   jobTitle: "(Programmer I)"
+  }
+};
+// Output that uses the data fields of the nested object
+console.log("Ticket", ticket.id, "was created on", ticket.dateCreated, "and assigned to employee", ticket.person.firstName, ticket.person.LastName, ticket.person.jobTitle);
